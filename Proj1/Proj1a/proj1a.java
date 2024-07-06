f9d5f72fc35179a671f71e085af6c5b3bb15eef7
André Dutra da Silva <116909759+dededutra@users.noreply.github.com>
2024-07-06 12:01:32 -0300

    Create proj1a.java

---
+++ Proj1/Proj1a/proj1a.java
@@ -0,0 +1,42 @@
+package projeto1;
+
+import java.util.Random;
+
+public class proj1a {
+
+    static final int TAM = 100;
+    static final int NUM = 1024;
+  
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {  
+        // Arrays para armazenar os tempos de processamento
+        double[] processingTimes = new double[NUM];
+
+        for (int i = 0; i < NUM; i++) {
+            int[] arr = new int[TAM];
+            Random rand = new Random();
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = rand.nextInt(10000);
+            }
+            long start = System.nanoTime();
+            insertionSort(arr);
+            long end = System.nanoTime();
+            double cpuTimeUsed = (end - start) / 1e9;
+            processingTimes[i] = cpuTimeUsed;
+        }
+        for (double time : processingTimes) {
+            System.out.println(time);
+        }
+    }
+}
