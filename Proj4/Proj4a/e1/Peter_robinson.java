f07032f29368faef91c7ae9d0d378aaa7aa8f9cf
André Dutra da Silva <116909759+dededutra@users.noreply.github.com>
2024-07-06 14:00:28 -0300

    Update Peter_robinson.java

--- Proj4/Proj4a/e1/Peter_robinson.java
+++ Proj4/Proj4a/e1/Peter_robinson.java
@@ -1,13 +1,13 @@
 public class Peter_robinson {
 
-    public static int peterRobinson(int m, int n) {
+    public static int funcPeterRobinson(int m, int n) {
         if (m == 0) return n + 1;
-        if (m > 0 && n == 0) return peterRobinson(m - 1, 1);
-        return peterRobinson(m - 1, peterRobinson(m, n - 1));
+        if (m > 0 && n == 0) return funcPeterRobinson(m - 1, 1);
+        return funcPeterRobinson(m - 1, funcPeterRobinson(m, n - 1));
     }
 
     public static void main(String[] args) {
         int m = 2, n = 3;
-        System.out.println("A(" + m + ", " + n + ") = " + peterRobinson(m, n));
+        System.out.println("A(" + m + ", " + n + ") = " + funcPeterRobinson(m, n));
     }
 }
