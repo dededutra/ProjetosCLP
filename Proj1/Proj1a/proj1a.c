b18a4ae5c125c3dff414bc58a801d35a2fc2e664
André Dutra da Silva <116909759+dededutra@users.noreply.github.com>
2024-07-06 11:47:46 -0300

    Update proj1a.c

--- Proj1/Proj1a/proj1a.c
+++ Proj1/Proj1a/proj1a.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAMANHO 100
-#define NUM_VETORES 1024
+#define TAM 100
+#define NUM 1024
 
 // Função para ordenação por inserção
 void insertionSort(int arr[], int n) {
@@ -11,9 +11,6 @@ void insertionSort(int arr[], int n) {
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
-
-        /* Move os elementos do arr[0..i-1], que são maiores que a chave,
-        para uma posição à frente de sua posição atual */
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -22,46 +19,23 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-// Função para gerar vetor aleatório
-void generateRandomArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 10000; // Números aleatórios entre 0 e 9999
-    }
-}
-
 int main() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios com a hora atual
-
+    srand(time(NULL));
     clock_t start, end;
     double cpu_time_used;
-
-    // Arrays para armazenar os tempos de processamento
-    double processing_times[ITERATIONS];
-
-    for (int i = 0; i < ITERATIONS; i++) {
-        int arr[SIZE];
-
-        // Gerar vetor aleatório
-        generateRandomArray(arr, SIZE);
-
-        // Registrar o tempo inicial
+    double processing_times[NUM];
+    for (int i = 0; i < NUM; i++) {
+        int arr[TAM];
+        for (int i = 0; i < TAM; i++) {
+            arr[i] = rand() % 10000;
+        }
         start = clock();
-
-        // Ordenar o vetor usando o algoritmo de ordenação por inserção
-        insertionSort(arr, SIZE);
-
-        // Registrar o tempo final
+        insertionSort(arr, TAM);
         end = clock();
-
-        // Calcular o tempo de processamento em segundos
         cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-        // Armazenar o tempo de processamento
         processing_times[i] = cpu_time_used;
     }
-
-    // Imprimir os tempos de processamento
-    for (int i = 0; i < ITERATIONS; i++) {
+    for (int i = 0; i < NUM; i++) {
         printf("%f\n",processing_times[i]);
     }
 
