47e706e7de752a4d9bf34bfca30cb83bf7c58678
André Dutra da Silva <116909759+dededutra@users.noreply.github.com>
2024-07-06 15:20:18 -0300

    Create bubblesort.lisp

---
+++ Proj4/Proj4a/e3/bubblesort.lisp
@@ -0,0 +1,32 @@
+(defun trocar (lista)
+  (if (null (cdr lista))
+      lista
+      (if (> (car lista) (cadr lista))
+          (cons (cadr lista) (trocar (cons (car lista) (cddr lista))))
+          (cons (car lista) (trocar (cdr lista))))))
+
+(defun bubble-sort (lista)
+  (if (null lista)
+      nil
+      (let ((troca (trocar lista)))
+        (if (equal lista troca)
+            lista
+            (bubble-sort troca)))))
+
+(defun ler-vetor ()
+  (let ((lista ()))
+    (format t "Digite os elementos da lista:~%")
+    (loop
+       (let ((entrada (read-line)))
+         (if (string= entrada "")
+             (return) ; Encerra o loop se a entrada estiver vazia
+             (handler-case
+                 (let ((numero (parse-integer entrada)))
+                   (push numero lista))
+               (parse-error ()
+                 (format t "Por favor, digite um número inteiro válido ou aperte Enter para terminar.~%"))))))
+    (nreverse lista)))
+
+(progn
+  (format t "~a~%" (bubble-sort 'ler-vetor))
+)
