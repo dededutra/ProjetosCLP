746e471d12a997d5ef919fc6a906199795b2bed7
André Dutra da Silva <116909759+dededutra@users.noreply.github.com>
2024-07-06 15:37:59 -0300

    Update Q32.lisp

--- Proj4/Proj4b/Q32.lisp
+++ Proj4/Proj4b/Q32.lisp
@@ -1,25 +1,21 @@
-;; Função para calcular o fatorial de um número não negativo n
+
 (defun fat (n)
   (cond
-    ((< n 0) nil)         ; Retorna nil se n for negativo
-    ((zerop n) 1)         ; Retorna 1 se n for zero
-    (t (* n (fat (- n 1)))))) ; Calcula n * (n-1) * ... * 1 recursivamente
+    ((< n 0) nil)         
+    ((zerop n) 1)         
+    (t (* n (fat (- n 1)))))) 
 
-;; Função para calcular a potência de b elevado a e (b^e)
 (defun poten (b e)
   (cond
-    ((< e 0) nil)         ; Retorna nil se e for negativo
-    ((zerop e) 1)         ; Retorna 1 se e for zero
-    (t (* b (poten b (- e 1)))))) ; Calcula b * b^(e-1) recursivamente
-
-;; Função para calcular a série exponencial modificada
+    ((< e 0) nil)         
+    ((zerop e) 1)         
+    (t (* b (poten b (- e 1)))))) 
 (defun exem (x n)
   (cond
-    ((< n 0) nil)                         ; Retorna nil se n for negativo
-    ((zerop n) 1)                         ; Retorna 1 se n for zero
-    (t (+ (/ (poten x n) (fat n))         ; Calcula x^n / n!
-           (exem x (- n 1))))))           ; Chama a função recursivamente para n-1
+    ((< n 0) nil)                         
+    ((zerop n) 1)                         
+    (t (+ (/ (poten x n) (fat n))         
+           (exem x (- n 1))))))           
 
-;; Imprime o resultado da série exponencial modificada para x=2 e n=4
-(format t "Para n=4 e x=2: ~d~%" (exem 2 4))
+(format t "~d~%" (exem 2 4))
 
